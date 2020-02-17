28a476c5a6fe1df25b536e852e769a587fd0f42c
Stephen Grable <stephengrable@Stephens-MBP.fios-router.home>
2020-02-17 12:48:50 -0500

    continue building class from script

--- packages/flux-vision/src/index.ts
+++ packages/flux-vision/src/index.ts
@@ -2,68 +2,81 @@ const defaultHTMLData = `<div style='display:none'> <div id="checkout-data" data
 
 export default class FluxVision {
   htmlDataElements: string;
+  currentStep: string;
+  currentPage: string;
+
   constructor({ htmlDataElements = defaultHTMLData }) {
     this.htmlDataElements = htmlDataElements;
+    this.currentStep = "contact_information";
+    this.currentStep = null;
+
     const body = document.querySelector("body");
     body.insertAdjacentHTML("beforeend", htmlDataElements);
+    this.currentEnvironment();
   }
-}
 
-// Analytics: FF main account
-const productAnalyticDatasetArray = [];
+  public sendAnalytics() {
+    const { currentStep } = this;
+    switch (currentStep) {
+      case "contact_information":
+        analytics.track("Checkout Started", {
+          order_id: dataset.orderNumber,
+          value: dataset.orderPrice,
+          currency: "USD",
+          products: productAnalyticDatasetArray,
+        });
+      case "shipping_method":
+        analytics.track("Checkout Step Viewed", {
+          checkout_id: dataset.checkoutId,
+          step: 2,
+          products: productAnalyticDatasetArray,
+        });
+      case "payment_method":
+        analytics.track("Checkout Step Viewed", {
+          checkout_id: dataset.checkoutId,
+          step: 3,
+          products: productAnalyticDatasetArray,
+        });
+      default:
+        break;
+    }
 
-const currentStep = Shopify.Checkout.step;
-const currentPage = Shopify.Checkout.page;
-
-// Get DOM elements with checkout data
-const dataset = document.querySelector("#checkout-data").dataset;
-const productsDatasets = document.querySelectorAll(
-  "#product-item-for-analytics-dataset",
-);
-
-for (const i = 0; i < productsDatasets.length; i++) {
-  const productDataset = productsDatasets[i].dataset;
-  if (productDataset) {
-    const formattedPrice = (productDataset.price / 100).toFixed(2);
-    productDataset.price = formattedPrice;
-    const objectProduct = Object.assign({}, productDataset);
-
-    productAnalyticDatasetArray.push(objectProduct);
+    if (currentPage == "thank_you") {
+      analytics.track("Order Completed", {
+        checkout_id: dataset.checkoutId,
+        order_id: dataset.orderId,
+        total: dataset.orderPrice,
+        currency: "USD",
+        products: productAnalyticDatasetArray,
+      });
+    }
   }
-}
 
-//   console.log('productAnalyticDatasetArray', productAnalyticDatasetArray)
+  private currentEnvironment() {
+    this.currentStep = Shopify.Checkout.step;
+    this.currentPage = Shopify.Checkout.page;
+  }
 
-switch (currentStep) {
-  case "contact_information":
-    analytics.track("Checkout Started", {
-      order_id: dataset.orderNumber,
-      value: dataset.orderPrice,
-      currency: "USD",
-      products: productAnalyticDatasetArray,
-    });
-  case "shipping_method":
-    analytics.track("Checkout Step Viewed", {
-      checkout_id: dataset.checkoutId,
-      step: 2,
-      products: productAnalyticDatasetArray,
-    });
-  case "payment_method":
-    analytics.track("Checkout Step Viewed", {
-      checkout_id: dataset.checkoutId,
-      step: 3,
-      products: productAnalyticDatasetArray,
-    });
-  default:
-    break;
-}
+  private pullDataFromDOM() {
+    // Analytics: FF main account
+    const productAnalyticDatasetArray = [];
 
-if (currentPage == "thank_you") {
-  analytics.track("Order Completed", {
-    checkout_id: dataset.checkoutId,
-    order_id: dataset.orderId,
-    total: dataset.orderPrice,
-    currency: "USD",
-    products: productAnalyticDatasetArray,
-  });
+    // Get DOM elements with checkout data
+    const dataset = document.querySelector("#checkout-data").dataset;
+    const productsDatasets = document.querySelectorAll(
+      "#product-item-for-analytics-dataset",
+    );
+
+    for (const i = 0; i < productsDatasets.length; i++) {
+      const productDataset = productsDatasets[i].dataset;
+      if (productDataset) {
+        const formattedPrice = (productDataset.price / 100).toFixed(2);
+        productDataset.price = formattedPrice;
+        const objectProduct = Object.assign({}, productDataset);
+
+        productAnalyticDatasetArray.push(objectProduct);
+      }
+    }
+    //   console.log('productAnalyticDatasetArray', productAnalyticDatasetArray)
+  }
 }
