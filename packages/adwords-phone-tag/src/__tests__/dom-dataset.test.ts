d9dd9a15ec21a4dd9bf39a30df73b36b00119716
Ian Ker-Seymer <ian.kerseymer@tryadhawk.com>
2020-03-06 23:54:37 -0500

    Lint

---
+++ packages/adwords-phone-tag/src/__tests__/dom-dataset.test.ts
@@ -0,0 +1,16 @@
+import DOMDataset from "./../dom-dataset";
+
+describe("DOMDataset", () => {
+  it("pulls data from a meta tag", () => {
+    document.body.innerHTML = `<meta itemprop=user data-name="Jane">`;
+    const domDataset = new DOMDataset("user");
+
+    expect(domDataset.dataset.name).toBe("Jane");
+  });
+
+  it("throws a error if meta element is not found", () => {
+    document.body.innerHTML = `<meta itemprop=unknown data-name="Jane">`;
+
+    expect(() => new DOMDataset("user")).toThrowError();
+  });
+});
