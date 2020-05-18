2808ca96d778ad7be0845dd8d3676f00f31e06c1
Ian Ker-Seymer <i.kerseymer@gmail.com>
2020-05-18 18:33:29 -0400

    Expand out visitor event prototype assignment for compat

--- packages/visitor-events/src/index.ts
+++ packages/visitor-events/src/index.ts
@@ -64,81 +64,90 @@ const VisitorEvents = integration("Visitor Events")
   .option("debug", false)
   .readyOnInitialize();
 
-Object.assign(VisitorEvents.prototype, {
-  normalizeEvent(event: any, type: string, argNames: string[] = []) {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { properties = {}, integrations: _, context, ...body } = event.obj;
-    const typeContext = {};
+VisitorEvents.prototype.normalizeEvent = function (
+  event: any,
+  type: string,
+  argNames: string[] = [],
+) {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { properties = {}, integrations: _, context, ...body } = event.obj;
+  const typeContext = {};
 
-    argNames.forEach(argName => {
-      // @ts-ignore
-      typeContext[argName] = body[argName];
-      delete body[argName];
-    });
-
-    const query = window.location.search;
-
-    // Add UTM params
-    if (query && !context.campaign) {
-      context.campaign = utm(query);
-    }
-
-    const normalized = {
-      ...body,
-      payload: properties,
-      type,
-      typeContext,
-      context,
-    };
+  argNames.forEach((argName) => {
     // @ts-ignore
-    this.debug("normalized event", normalized);
-    return normalized;
-  },
-  initialize() {
-    this.queue = new Queue("visitor-events", (item: any, done: any) => {
-      return this.postJSON(item, done)
-        .then(res => done(null, res))
-        .catch((err: Error) => {
-          console.error(err);
-          done(err);
-        });
-    });
-    this.queue.start();
-    // @ts-ignore
-    this.ready();
-  },
-  track(event: any) {
-    return this.publishEvent(event, "track");
-  },
-  page(event: any) {
-    return this.publishEvent(event, "page", ["name", "category"]);
-  },
-  identify(event: any) {
-    return this.publishEvent(event, "identify");
-  },
-  group(event: any) {
-    return this.publishEvent(event, "group", ["groupId"]);
-  },
-  alias(event: any) {
-    return this.publishEvent(event, "alias", ["previousId"]);
-  },
-  publishEvent(event: any, type: any, argNames: string[] = []) {
-    return this.queue.addItem(this.normalizeEvent(event, type, argNames));
-  },
-  getQueue() {
-    return this.queue;
-  },
-  postJSON(data: object, done: any): Promise<Response> {
-    const request = {
-      method: "POST",
-      credentials: "same-origin" as "same-origin",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(data),
-    };
+    typeContext[argName] = body[argName];
+    delete body[argName];
+  });
 
-    // @ts-ignore
-    return postJSON(this.options.endpoint, request, done);
-  },
-});
+  const query = window.location.search;
+
+  // Add UTM params
+  if (query && !context.campaign) {
+    context.campaign = utm(query);
+  }
+
+  const normalized = {
+    ...body,
+    payload: properties,
+    type,
+    typeContext,
+    context,
+  };
+  // @ts-ignore
+  this.debug("normalized event", normalized);
+  return normalized;
+};
+VisitorEvents.prototype.initialize = function () {
+  this.queue = new Queue("visitor-events", (item: any, done: any) => {
+    return this.postJSON(item, done)
+      .then((res) => done(null, res))
+      .catch((err: Error) => {
+        console.error(err);
+        done(err);
+      });
+  });
+  this.queue.start();
+  // @ts-ignore
+  this.ready();
+};
+VisitorEvents.prototype.track = function (event: any) {
+  return this.publishEvent(event, "track");
+};
+VisitorEvents.prototype.page = function (event: any) {
+  return this.publishEvent(event, "page", ["name", "category"]);
+};
+VisitorEvents.prototype.identify = function (event: any) {
+  return this.publishEvent(event, "identify");
+};
+VisitorEvents.prototype.group = function (event: any) {
+  return this.publishEvent(event, "group", ["groupId"]);
+};
+VisitorEvents.prototype.alias = function (event: any) {
+  return this.publishEvent(event, "alias", ["previousId"]);
+};
+VisitorEvents.prototype.publishEvent = function (
+  event: any,
+  type: any,
+  argNames: string[] = [],
+) {
+  return this.queue.addItem(this.normalizeEvent(event, type, argNames));
+};
+VisitorEvents.prototype.getQueue = function () {
+  return this.queue;
+};
+VisitorEvents.prototype.postJSON = function (
+  data: object,
+  done: any,
+): Promise<Response> {
+  const request = {
+    method: "POST",
+    credentials: "same-origin" as "same-origin",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  };
+
+  // @ts-ignore
+  return postJSON(this.options.endpoint, request, done);
+};
 
 export default VisitorEvents;
