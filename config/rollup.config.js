f8ccea1099a1b0e7a962f1dbaaeec65871ee3b1e
Ian Ker-Seymer <i.kerseymer@gmail.com>
2020-10-12 17:06:32 -0400

    Attempt to fix minification

--- config/rollup.config.js
+++ config/rollup.config.js
@@ -1,6 +1,5 @@
 /* eslint-disable */
 import nodeResolve from "@rollup/plugin-node-resolve";
-import invariantPlugin from "rollup-plugin-invariant";
 import commonjs from "@rollup/plugin-commonjs";
 import { terser as minify } from "rollup-plugin-terser";
 import babel from "@rollup/plugin-babel";
@@ -16,19 +15,7 @@ export function rollup({
     return `./lib/${name}.umd${postfix}.js`;
   }
 
-  const minifyPlugin = minify({
-    mangle: {
-      toplevel: true,
-    },
-    output: {
-      comments: false,
-    },
-    compress: {
-      global_defs: {
-        "@process.env.NODE_ENV": JSON.stringify("production"),
-      },
-    },
-  });
+  const minifyPlugin = minify();
 
   function fromSource(format, out = format, opts = { plugins: [] }) {
     if (skipFormats.includes(format)) {
@@ -37,15 +24,11 @@ export function rollup({
 
     return {
       input,
-      external: ["react"],
       output: {
         file: outputFile(out),
         format,
         sourcemap: true,
         name: umdName,
-        globals: {
-          react: "React",
-        },
       },
       plugins: [
         nodeResolve({
@@ -53,15 +36,6 @@ export function rollup({
           mainFields: ["browser", "jsnext", "module", "main"],
         }),
         commonjs(),
-        invariantPlugin({
-          // Instead of completely stripping InvariantError messages in
-          // production, this option assigns a numeric code to the
-          // production version of each error (unique to the call/throw
-          // location), which makes it much easier to trace production
-          // errors back to the unminified code where they were thrown,
-          // where the full error string can be found. See #4519.
-          errorCodes: true,
-        }),
         babel({
           babelHelpers: "bundled",
           exclude: "node_modules/**",
